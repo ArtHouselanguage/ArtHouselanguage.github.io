885a0810744345d1b92ba0c0dcf6423d05b607ff
ArtHouselanguage <118930400+ArtHouselanguage@users.noreply.github.com>
2022-11-23 10:36:53 -0800

    Add files via upload

---
+++ js/br.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+const drop=document.getElementById('dropdown');
+/*const allLang = ['en', 'ru', 'am']
+drop.addEventListener('change', changeUrLLanguage)
+
+function changeUrLLanguage(){
+
+    let lang=drop.value;
+    location.href = window.location.pathname + '#'+lang;
+    location.reload()
+}
+function changeLanguage(){
+    let hash = window.location.hash;
+    hash= hash.substr(1);
+    if(!allLang.includes(hash)){
+        location.href = window.location.pathname + '#en';
+        location.reload()
+
+    }
+    Selection.value= hash;
+
+
+}
+changeLanguage()*/
---
+++ js/lang.js
@@ -0,0 +1,25 @@
+
+const langArr={
+
+
+
+"unit":{
+    "am": "Մեր մասին",
+    "en": "About us",
+    "ru": "O нас",
+
+},
+
+"choice": {
+"am": "Ընտրել դասընթացը",
+"en":"Choose the lesson",
+"ru": "выбрать урок",
+
+
+}
+
+
+
+
+
+}
